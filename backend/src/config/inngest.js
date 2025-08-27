6fa2ec7fd3a00ed5b28d7a6495dcb7faae43acc3
im-apratap <167358352+im-apratap@users.noreply.github.com>
2025-08-28 03:01:58 +0530

    try

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "video-conferencing" });
 
 const syncUser = inngest.createFunction(
     {id: "sync-user"},
-    {event: "user.created"},
+    {event: "clerk/user.created"},
     async({event})=>{
         await connectDB()
 
@@ -28,7 +28,7 @@ const syncUser = inngest.createFunction(
 
 const deleteUserFromDB = inngest.createFunction(
     {id: "delete-user-from-db"},
-    {event: "user.deleted"},
+    {event: "clerk/user.deleted"},
     async({event})=>{
         await connectDB()
 
